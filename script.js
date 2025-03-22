c8b171df2f1a674e440c1b4a638091e8501c99eb
Justin Tapia <75755009+justjust1n1126@users.noreply.github.com>
2025-03-22 15:08:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 function create() {
-  let circle1 = this.add.circle(50, 100, 90, 0xFFF070);
-  let circle2 = this.add.circle(95, 300, 80, 0xFF0000);
-  let circle3 = this.add.circle(200, 200, 100, 0x9F00D0);
-  let circle4 = this.add.circle(300, 400, 10, 0x00E030);
-  let circle5 = this.add.circle(190, 200, 50, 0xFFEF40);
+  const width = this.sys.game.config.width;
+  const height = this.sys.game.config.height;
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function addRandomCircles(numCircles) {
+    for (let i = 0; i < numCircles; i++) {
+      let x = Math.random() * width;
+      let y = Math.random() * height;
+      let radius = Math.random() * 100 + 20;
+      let color = getRandomColor();
+      this.add.circle(x, y, radius, color);
+    }
+  }
+
+  addRandomCircles.call(this, 50); 
 }
 
 const config = {
-	type: Phaser.AUTO,
-	width: 1000,
-	height: 1000,
-	scene: {
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 1000,
+  scene: {
     create
-	}
+  }
 }
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
