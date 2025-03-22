75f6840740c0f0c54ce27578c521bb660e4d144d
Justin Tapia <75755009+justjust1n1126@users.noreply.github.com>
2025-03-22 14:56:05 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function create() {
+  let circle1 = this.add.circle(50, 100, 90, 0xFFF070);
+  let circle2 = this.add.circle(95, 300, 80, 0xFF0000);
+  let circle3 = this.add.circle(200, 200, 100, 0x9F00D0);
+  let circle4 = this.add.circle(300, 400, 10, 0x00E030);
+  let circle5 = this.add.circle(190, 200, 50, 0xFFEF40);
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 1000,
+	height: 1000,
+	scene: {
+    create
+	}
+}
+
+const game = new Phaser.Game(config)
